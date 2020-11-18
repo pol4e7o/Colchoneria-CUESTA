eb527c5539f99b3505ddb50592d7675614645940
pol4e7o <74629093+pol4e7o@users.noreply.github.com>
2020-11-18 21:10:28 +0100

    Pantalla principal terminada

--- Colchoneria_CUESTA/Colchoneria_CUESTA/Formulario_Basico.Designer.vb
+++ Colchoneria_CUESTA/Colchoneria_CUESTA/Formulario_Basico.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Pantalla_principal
+Partial Class Formulario_Basico
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -23,7 +23,6 @@ Partial Class Pantalla_principal
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Pantalla_principal))
         Me.Menu = New System.Windows.Forms.MenuStrip()
         Me.InicioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.CerrarCajaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -33,13 +32,11 @@ Partial Class Pantalla_principal
         Me.VentaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AcercaDeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.TimerHoraReal = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.label_ayuda = New System.Windows.Forms.Label()
         Me.label_fecha = New System.Windows.Forms.Label()
         Me.label_hora = New System.Windows.Forms.Label()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.label_saludo = New System.Windows.Forms.Label()
-        Me.label_pulsarTecla = New System.Windows.Forms.Label()
+        Me.TimerHoraReal = New System.Windows.Forms.Timer(Me.components)
         Me.Menu.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
@@ -47,106 +44,127 @@ Partial Class Pantalla_principal
         'Menu
         '
         Me.Menu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InicioToolStripMenuItem, Me.VentaToolStripMenuItem, Me.AcercaDeToolStripMenuItem, Me.AyudaToolStripMenuItem})
-        resources.ApplyResources(Me.Menu, "Menu")
+        Me.Menu.Location = New System.Drawing.Point(0, 0)
         Me.Menu.Name = "Menu"
+        Me.Menu.Size = New System.Drawing.Size(800, 24)
+        Me.Menu.TabIndex = 1
+        Me.Menu.Text = "MenuStrip1"
         '
         'InicioToolStripMenuItem
         '
         Me.InicioToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CerrarCajaToolStripMenuItem, Me.ToolStripSeparator2, Me.VolverToolStripMenuItem, Me.SalirToolStripMenuItem})
         Me.InicioToolStripMenuItem.Name = "InicioToolStripMenuItem"
-        resources.ApplyResources(Me.InicioToolStripMenuItem, "InicioToolStripMenuItem")
+        Me.InicioToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.InicioToolStripMenuItem.Text = "Inicio"
         '
         'CerrarCajaToolStripMenuItem
         '
         Me.CerrarCajaToolStripMenuItem.Name = "CerrarCajaToolStripMenuItem"
-        resources.ApplyResources(Me.CerrarCajaToolStripMenuItem, "CerrarCajaToolStripMenuItem")
+        Me.CerrarCajaToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.CerrarCajaToolStripMenuItem.Text = "Cerrar caja"
         '
         'ToolStripSeparator2
         '
         Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
-        resources.ApplyResources(Me.ToolStripSeparator2, "ToolStripSeparator2")
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(127, 6)
         '
         'VolverToolStripMenuItem
         '
         Me.VolverToolStripMenuItem.Name = "VolverToolStripMenuItem"
-        resources.ApplyResources(Me.VolverToolStripMenuItem, "VolverToolStripMenuItem")
+        Me.VolverToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.VolverToolStripMenuItem.Text = "Volver"
         '
         'SalirToolStripMenuItem
         '
         Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
-        resources.ApplyResources(Me.SalirToolStripMenuItem, "SalirToolStripMenuItem")
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(130, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
         '
         'VentaToolStripMenuItem
         '
         Me.VentaToolStripMenuItem.Name = "VentaToolStripMenuItem"
-        resources.ApplyResources(Me.VentaToolStripMenuItem, "VentaToolStripMenuItem")
+        Me.VentaToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.VentaToolStripMenuItem.Text = "Venta"
         '
         'AcercaDeToolStripMenuItem
         '
         Me.AcercaDeToolStripMenuItem.Name = "AcercaDeToolStripMenuItem"
-        resources.ApplyResources(Me.AcercaDeToolStripMenuItem, "AcercaDeToolStripMenuItem")
+        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
+        Me.AcercaDeToolStripMenuItem.Text = "Acerca de..."
         '
         'AyudaToolStripMenuItem
         '
         Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
-        resources.ApplyResources(Me.AyudaToolStripMenuItem, "AyudaToolStripMenuItem")
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.label_ayuda)
+        Me.Panel1.Controls.Add(Me.label_fecha)
+        Me.Panel1.Controls.Add(Me.label_hora)
+        Me.Panel1.Location = New System.Drawing.Point(0, 408)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 42)
+        Me.Panel1.TabIndex = 5
+        '
+        'label_ayuda
+        '
+        Me.label_ayuda.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.label_ayuda.AutoSize = True
+        Me.label_ayuda.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.label_ayuda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label_ayuda.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.label_ayuda.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.label_ayuda.Location = New System.Drawing.Point(10, 9)
+        Me.label_ayuda.Name = "label_ayuda"
+        Me.label_ayuda.Size = New System.Drawing.Size(88, 22)
+        Me.label_ayuda.TabIndex = 1
+        Me.label_ayuda.Text = "F1 - Ayuda"
+        Me.label_ayuda.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'label_fecha
+        '
+        Me.label_fecha.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.label_fecha.AutoSize = True
+        Me.label_fecha.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label_fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.label_fecha.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.label_fecha.Location = New System.Drawing.Point(455, 9)
+        Me.label_fecha.Name = "label_fecha"
+        Me.label_fecha.Size = New System.Drawing.Size(64, 22)
+        Me.label_fecha.TabIndex = 2
+        Me.label_fecha.Text = "Fecha: "
+        '
+        'label_hora
+        '
+        Me.label_hora.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.label_hora.AutoSize = True
+        Me.label_hora.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.label_hora.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.label_hora.ImeMode = System.Windows.Forms.ImeMode.NoControl
+        Me.label_hora.Location = New System.Drawing.Point(646, 9)
+        Me.label_hora.Name = "label_hora"
+        Me.label_hora.Size = New System.Drawing.Size(54, 22)
+        Me.label_hora.TabIndex = 3
+        Me.label_hora.Text = "Hora: "
         '
         'TimerHoraReal
         '
         Me.TimerHoraReal.Interval = 1000
         '
-        'label_ayuda
+        'Formulario_Basico
         '
-        resources.ApplyResources(Me.label_ayuda, "label_ayuda")
-        Me.label_ayuda.BackColor = System.Drawing.SystemColors.ButtonFace
-        Me.label_ayuda.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.label_ayuda.Name = "label_ayuda"
-        '
-        'label_fecha
-        '
-        resources.ApplyResources(Me.label_fecha, "label_fecha")
-        Me.label_fecha.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.label_fecha.Name = "label_fecha"
-        '
-        'label_hora
-        '
-        resources.ApplyResources(Me.label_hora, "label_hora")
-        Me.label_hora.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.label_hora.Name = "label_hora"
-        '
-        'Panel1
-        '
-        resources.ApplyResources(Me.Panel1, "Panel1")
-        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Panel1.Controls.Add(Me.label_ayuda)
-        Me.Panel1.Controls.Add(Me.label_fecha)
-        Me.Panel1.Controls.Add(Me.label_hora)
-        Me.Panel1.Name = "Panel1"
-        '
-        'label_saludo
-        '
-        resources.ApplyResources(Me.label_saludo, "label_saludo")
-        Me.label_saludo.BackColor = System.Drawing.SystemColors.ButtonFace
-        Me.label_saludo.Name = "label_saludo"
-        '
-        'label_pulsarTecla
-        '
-        resources.ApplyResources(Me.label_pulsarTecla, "label_pulsarTecla")
-        Me.label_pulsarTecla.BackColor = System.Drawing.SystemColors.ButtonFace
-        Me.label_pulsarTecla.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.label_pulsarTecla.Name = "label_pulsarTecla"
-        '
-        'Pantalla_principal
-        '
-        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.label_pulsarTecla)
-        Me.Controls.Add(Me.label_saludo)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Menu)
-        Me.MainMenuStrip = Me.Menu
-        Me.Name = "Pantalla_principal"
-        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Name = "Formulario_Basico"
+        Me.Text = "Formulario_Basico"
         Me.Menu.ResumeLayout(False)
         Me.Menu.PerformLayout()
         Me.Panel1.ResumeLayout(False)
@@ -158,18 +176,16 @@ Partial Class Pantalla_principal
 
     Friend WithEvents Menu As MenuStrip
     Friend WithEvents InicioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarCajaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
     Friend WithEvents VolverToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents VentaToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AcercaDeToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents TimerHoraReal As Timer
+    Friend WithEvents Panel1 As Panel
     Friend WithEvents label_ayuda As Label
     Friend WithEvents label_fecha As Label
     Friend WithEvents label_hora As Label
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents VentaToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents CerrarCajaToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
-    Friend WithEvents label_saludo As Label
-    Friend WithEvents label_pulsarTecla As Label
+    Friend WithEvents TimerHoraReal As Timer
 End Class
--- Colchoneria_CUESTA/Colchoneria_CUESTA/Pantalla_de_inicio.vb
+++ Colchoneria_CUESTA/Colchoneria_CUESTA/Pantalla_de_inicio.vb
@@ -1,6 +1,5 @@
-﻿Public Class Pantalla_principal
-
-    Private Sub Pantalla_principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Public Class Pantalla_de_inicio
+    Private Sub Pantalla_de_inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         'Se indica la fecha actual
         label_fecha.Text = "Fecha: " & DateString
@@ -17,11 +16,4 @@
 
     End Sub
 
-    Private Sub Pantalla_principal_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
-
-        'Si se pulsa un boton del teclado se carga el siguente formulario
-        Pantalla_de_inicio.Show()
-        Me.Hide()
-
-    End Sub
 End Class
