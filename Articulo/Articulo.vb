1727de52e5ca579d7d41e99b1c999230a536137a
Ozymandias2396 <74670516+Ozymandias2396@users.noreply.github.com>
2020-11-18 20:51:18 +0100

    Clases

---
+++ Articulo/Articulo.vb
@@ -0,0 +1,62 @@
+﻿Public Class Articulo
+    'Variables de cada atributo
+    Private nombreArticulo As String
+    Private descripcionArticulo As String
+    Private categoriaArticulo As String
+    Private tamanioArticulo As ArrayList
+    Private precioArticulo As ArrayList
+    Private categoriasArticulo As ArrayList
+
+    'Funciones getters para obtener los valores del objeto
+    Function getnombreArticulo() As String
+        Return nombreArticulo
+    End Function
+
+    Function getdescripcionArticulo() As String
+        Return descripcionArticulo
+    End Function
+
+    Function getcategoriaArticulo() As String
+        Return categoriaArticulo
+    End Function
+
+    Function gettamanioArticulo() As ArrayList
+        Return tamanioArticulo
+    End Function
+
+    Function getprecioArticulo() As ArrayList
+        Return precioArticulo
+    End Function
+
+    Function getcategoriasArticulo() As ArrayList
+        Return categoriasArticulo
+    End Function
+
+    'Funciones setter para modificar el valor de las variables del objeto
+    Function setnombreArticulo(nombre As String) As String
+        nombreArticulo = nombre
+    End Function
+
+    Function setdescripcionArticulo(descripcion As String) As String
+        descripcionArticulo = descripcion
+    End Function
+
+    Function setcategoriaArticulo(categoria As String) As String
+        categoriaArticulo = categoria
+    End Function
+
+    Function settamanioArticulo(tamanio As ArrayList) As ArrayList
+        tamanioArticulo = tamanio
+    End Function
+
+    Function getprecioArticulo(precio As ArrayList) As ArrayList
+        precioArticulo = precio
+    End Function
+
+    Function setcategoriasArticulo(categorias As ArrayList) As ArrayList
+        categoriasArticulo = categorias
+    End Function
+
+
+
+End Class
